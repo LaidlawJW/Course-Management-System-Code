158e3db88cf3ce5d4de439e1f6cae7b1a64471cf
colten <coltendoesstuff@gmail.com>
2022-04-13 16:05:10 -0500

    Modified php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,13 +1,10 @@
 <?php
-if(isset($_POST["submitInfoButton"])){
+if(isset($_POST["submit"])){
     echo "good good";
 
     $name= $_POST["uname"];
     $psw= $_POST["pws"];
     $repeatPws= $_POST["repeatPsw"];
-    
-
-
 }
 else{
     header("location: ../login.php");
--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,12 +1,15 @@
 <?php
 include_once 'includes/dbh.php';
+include_once 'includes/helper.php';
+console_log(['ass']);
 
 $name = $_POST['uname'];
 $pass = $_POST['psw'];
 $pass2 = $_POST['repeatPsw'];
 
+if ($pass != $pass2) header("Location: ../login.php?error=pwdMatchError");
 
-$sql = "INSERT INTO main( '$name', '$pass', '$pass2');";
+$sql = "INSERT INTO main (name, password) VALUES ('$name', '$pass', '$pass2');";
 
 //$sql = "INSERT INTO MyGuests (firstname, lastname, email)VALUES ('John', 'Doe', 'john@example.com')";
 
