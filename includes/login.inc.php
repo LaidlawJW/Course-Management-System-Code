4bfad3accf53d01c368391ad972d08d4886ed7ae
colten <coltendoesstuff@gmail.com>
2022-05-02 17:09:21 -0500

    login system works after many weeks of errors due to blood sweat and tears

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,42 +1,36 @@
 <?php
 include_once 'dbh.php';
 include_once 'helper.php';
-session_start();
 
 $name = $_POST['uname'];
 $pass = $_POST['psw'];
-$_SESSION["uname"] = "$name";
-$_SESSION["pwd"] = "$pwd";
-
-console_log('bet');
-
-
 
 if(isset($_POST["submit"])){
     echo "good good";
+    echo $echo;
 
-    $SQLPasswordQuery = "SELECT pwd FROM main WHERE UserID = $name;";
-    $SQLPassword = mysqli_query($conn,$SQLPasswordQuery, MYSQLI_USE_RESULT);
-
-    $SQLUserID =  "SELECT UserID FROM main WHERE password = $pass;";
-
-    
-
-    echo $SQLPassword;
-    // echo $SQLUserID;
-    // echo $name;
-    // echo $pass;
-    
-
-    if($name = $SQLUserID && $pass = $SQLPassword){
-        header("location: ../pages/Dashboard_temp.php");
-
-
+    try {
+        $SQLUser =  "SELECT * FROM main WHERE username = '$name';";
+        $result = mysqli_query($conn, $SQLUser);
+        $rows = mysqli_fetch_assoc($result);
+    } catch(Exception $e) {
+        die($e->getMessage());
     }
-    //mysqli_query($conn, $sql);
 
+    $password = $rows['password'];
 
-}
-else{
-    echo" oh no your web dev is bad";
+    if($pass == $password){
+        session_start();
+        $_SESSION["UserID"] = "$UserID";
+        $_SESSION["uname"] = "$name";
+    
+        header("location: ../pages/Dashboard_temp.php");
+        exit();
+    }
+    else{
+        console_log('ahhhhh');
+
+        echo" oh no your web dev is bad";
+        //header("location: /signout.inc.php");
+    }
 }
