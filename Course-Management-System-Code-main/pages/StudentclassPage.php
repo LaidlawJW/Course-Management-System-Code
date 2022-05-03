9e234c3a392493c058b596616b45dc92501fe039
Justin Laidlaw <justin.laidlaw102@gmail.com>
2022-05-02 20:26:18 -0500

    2 of 2 Nav bar link destinations fixed, DoB info slot removed from profile

---
+++ Course-Management-System-Code-main/pages/StudentclassPage.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/787fe15a79.js" crossorigin="anonymous"></script>
+    <script src="../JS/main.js"></script>
+    <link rel="stylesheet" href="../CSS/Main.css">
+    <title>Document</title>
+</head>
+
+<body>
+<?php include_once('navigation.php'); ?>
+
+    <div class="col-2">
+        <header>
+            <h2>Current Date: <span id='date'></span></h2>
+            <h2>Class Page Name</h2>
+            <h2>Hello <span id='user'><?php
+            echo $_SESSION["uname"];
+
+        
+            ?></span>!</h2>
+        </header>
+
+        <main class="t-dash">
+
+            <div class="t-nav">
+                <a class="t-nav_item" href="Submissions.php">
+                        Submissions 
+                    </a>
+                <a class="t-nav_item" href="viewPpl.php">
+                        View people 
+                    </a>
+            </div>
+
+            <div class="t-cards">
+
+                <div class="card">
+                    <div class="course-name-social">
+                        <h5>Course</h5>
+                        <a href="StudentclassPage.php">
+                            <h4>Ethical Reasoning</h4>
+                        </a>
+                    </div>
+
+                    <div class="course-info">
+                        <h5>Due Date</h5>
+                        <a href="Do-ass.php">
+                            <h4>Assignment 01</h4>
+                        </a>
+                    </div>
+                    <div class="points">
+                        <div class="frame">
+                            <h3>PTS: <span id="PTS">100</span></h3>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="course-name-social">
+                        <h5>Course</h5>
+                        <a href="StudentclassPage.php">
+                            <h4>Ethical Reasoning</h4>
+                        </a>
+                    </div>
+
+                    <div class="course-info">
+                        <h5>Due Date</h5>
+                        <a href="Do-ass.php">
+                            <h4>Assignment 02</h4>
+                        </a>
+                    </div>
+                    <div class="points">
+                        <div class="frame">
+                            <h3>PTS: <span id="PTS">100</span></h3>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="course-name-social">
+                        <h5>Course</h5>
+                        <a href="StudentclassPage.php">
+                            <h4>Ethical Reasoning</h4>
+                        </a>
+                    </div>
+
+                    <div class="course-info">
+                        <h5>Due Date</h5>
+                        <a href="Do-ass.php">
+                            <h4>Assignment 03</h4>
+                        </a>
+                    </div>
+                    <div class="points">
+                        <div class="frame">
+                            <h3>PTS: <span id="PTS">100</span></h3>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="course-name-social">
+                        <h5>Course</h5>
+                        <a href="StudentclassPage.php">
+                            <h4>Ethical Reasoning</h4>
+                        </a>
+                    </div>
+
+                    <div class="course-info">
+                        <h5>Due Date</h5>
+                        <a href="Do-ass.php">
+                            <h4>Assignment 04</h4>
+                        </a>
+                    </div>
+                    <div class="points">
+                        <div class="frame">
+                            <h3>PTS: <span id="PTS">100</span></h3>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="course-name-social">
+                        <h5>Course</h5>
+                        <a href="StudentclassPage.php">
+                            <h4>Ethical Reasoning</h4>
+                        </a>
+                    </div>
+
+                    <div class="course-info">
+                        <h5>Due Date</h5>
+                        <a href="Do-ass.php">
+                            <h4>Assignment 05</h4>
+                        </a>
+                    </div>
+                    <div class="points">
+                        <div class="frame">
+                            <h3>PTS: <span id="PTS">100</span></h3>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="course-name-social">
+                        <h5>Course</h5>
+                        <a href="StudentclassPage.php">
+                            <h4>Ethical Reasoning</h4>
+                        </a>
+                    </div>
+
+                    <div class="course-info">
+                        <h5>Due Date</h5>
+                        <a href="Do-ass.php">
+                            <h4>Assignment 06</h4>
+                        </a>
+                    </div>
+                    <div class="points">
+                        <div class="frame">
+                            <h3>PTS: <span id="PTS">100</span></h3>
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+
+        </main>
+        <footer></footer>
+    </div>
+    <aside></aside>
+</body>
+
+<script>
+    var dt = new Date();
+    var NoTimeDate = dt.getFullYear() + '/' + (dt.getMonth() + 1) + '/' + dt.getDate();
+    document.getElementById('date').innerHTML = NoTimeDate;
+</script>
+
+
+
+</html>
---
+++ Course-Management-System-Code-main/pages/profilePage_temp.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+
+<?php
+session_start();
+
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/787fe15a79.js" crossorigin="anonymous"></script>
+    <script src="../JS/main.js"></script>
+    <link rel="stylesheet" href="../CSS/Main.css">
+    <title>Document</title>
+</head>
+
+<body>
+<?php include_once('navigation.php'); ?>
+
+    <div class="col-2">
+        <header>
+            <h2>Current Date: <span id='date'></span></h2>
+            <h2>Profile</h2>
+            <h2>Hello <span id='user'>
+            <?php
+            echo $_SESSION["uname"];
+
+        
+            ?></span>!</h2>
+        </header>
+        <main class='main-prof'>
+            <div class="container">
+                <br>
+                <a href="edit-prof.php" class="btn-edit">Edit Your Profile</a>
+                <br>
+                <br>
+                <div class="row">
+                    <div class="profile">
+                        <figure>
+                            <img src="../img/photo-man.jpg" />
+                        </figure>
+                        <h1><?php
+            echo $_SESSION["uname"];
+
+        
+            ?>!</h1>
+                        <table>
+                            <dl>
+                                <dt>Full name</dt>
+                                <dd><?php
+            echo $_SESSION["uname"];
+
+        
+            ?>!</dd>
+                                <dt>Degree</dt>
+                                <dd>Computer Science</dd>
+                                <dt>Email</dt>
+                                <dd><?php
+            echo $_SESSION["uname"];
+
+        
+            ?>@gmail.com</dd>
+                            </dl>
+                        </table>
+
+                    </div>
+                </div>
+
+
+
+
+                <div class="prof-classes">
+                    <table>
+                        <tr>
+                            <th>Current Classes</th>
+                            <th>Role</th>
+                        </tr>
+                        <tr>
+                            <td><a href="StudentclassPage.php">Ethics</a></td>
+                            <td>Student</td>
+                        </tr>
+                        <tr>
+                            <td><a href="StudentclassPage.php">Biology</a></td>
+                            <td>Student</td>
+                        </tr>
+                        <tr>
+                            <td><a href="StudentclassPage.php">Programming languages</a></td>
+                            <td>Student</td>
+                        </tr>
+                        <tr>
+                            <td><a href="StudentclassPage.php">Calc 2</a></td>
+                            <td>Student</td>
+                        </tr>
+                        <tr>
+                            <td><a href="StudentclassPage.php">Art History</a></td>
+                            <td>Student</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </main>
+        <footer></footer>
+    </div>
+    <aside></aside>
+</body>
+
+<script>
+    var dt = new Date();
+    var NoTimeDate = dt.getFullYear() + '/' + (dt.getMonth() + 1) + '/' + dt.getDate();
+    document.getElementById('date').innerHTML = NoTimeDate;
+</script>
+
+
+
+</html>
